a94be3c33a2fd3aa5cf6da15a21109663f879ef1
ahmadPervaviz <aplahore4@gmail.com>
2022-11-24 11:43:01 +0500

    redux implemented.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,28 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
 import classes from './Counter.module.css'
+import { counterActions } from '../store/counter'
 
 const Counter = () => {
   const dispatch = useDispatch()
+
   const counter = useSelector((state) => {
-    return state.counter
+    return state.counter.counter
   })
   const show = useSelector((state) => {
-    return state.showCounter
+    return state.counter.showCounter
   })
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   }
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 10 })
+    dispatch(counterActions.increase({ type: 'increase', amount: 10 }))
   }
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   }
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
   }
 
   return (
